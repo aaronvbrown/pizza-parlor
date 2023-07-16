776b88200422714fbc8cb0fb54712cac1a332dae
Aaron Brown <worldofaaron@hotmail.com>
2023-07-15 20:10:20 -0700

    add event listener for updating order

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 // Business Logic
 
 const sizesAvailable = {
-    'small':    10.00,
-    'medium':   12.50,
-    'large':    15.00
+    'small': 10.00,
+    'medium': 12.50,
+    'large': 15.00
 };
-const toppingsAvailable = ['olives', 'mushrooms', 'onions', 'peppers', 'tomatoes', 'pineapple','pepperoni', 'sausage', 'ham'];
+const toppingsAvailable = ['olives', 'mushrooms', 'onions', 'peppers', 'tomatoes', 'pineapple', 'pepperoni', 'sausage', 'ham'];
 
 //class method
 // class Pizza {
@@ -29,9 +29,9 @@ function Pizza(size, toppings) {
     this.toppings = toppings;
 }
 
-Pizza.prototype.pricePizza = function() {
+Pizza.prototype.pricePizza = function () {
     let basePrice = sizesAvailable[this.size];
-    let topppingsPrice = this.toppings.length * basePrice/10;
+    let topppingsPrice = this.toppings.length * basePrice / 10;
     let salesTax = .08 * (basePrice + topppingsPrice);
     let totalPrice = basePrice + topppingsPrice + salesTax;
     totalPrice = totalPrice.toFixed(2);
@@ -42,29 +42,52 @@ Pizza.prototype.pricePizza = function() {
 // User Interface Logic
 // generate checkboxes for toppings
 
-//get the container that will hold the checkboxes
-function populateToppings () {
+function populateToppings() {
+    //get the container that will hold the checkboxes
     const toppingsContainer = document.getElementById('toppingsForm');
-    
+
     toppingsAvailable.forEach((topping) => {
         //create new checkbox element
         const checkbox = document.createElement('input');
-        checkbox.type='checkbox';
-        checkbox.value=topping;
-    
+        checkbox.type = 'checkbox';
+        checkbox.value = topping;
+
         //create label for checkbox
         const label = document.createElement('label');
         label.textContent = topping;
 
         //create a break
         const lineBreak = document.createElement('br');
-    
+
         //append the checkbox, label and break to the container element
         toppingsContainer.appendChild(checkbox);
         toppingsContainer.appendChild(label);
         toppingsContainer.appendChild(lineBreak);
     });
 }
+
+// this listener failed entirely.  Does it just not like the names?
+// window.addEventListener("load", function () {
+//     let updateBtn = document.getElementById("update");
+//     let displayOrder = document.getElementById("displayOrder");
+    
+//     updateBtn.addEventListener("click", function () {
+//         displayOrder.removeAttribute("class");
+//         preventDefault();
+
+//     })
+
+// })
+
+// this listener works.  All I really changed were the names and identifications
+window.addEventListener("load", function() {
+    let updateOrderBtn = document.getElementById("orderButton");
+    let order = document.getElementById("displayOrder");
+
+    updateOrderBtn.addEventListener("click", function () {
+        order.removeAttribute("class");
+    })
+})
 
 
 
